ebe78bffa75c82525eaf57daadfdb2d4af69ff04
wizel10 <wizel10@gmail.com>
2024-12-12 12:05:56 +0100

    build and test

---
+++ hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d
@@ -0,0 +1,5 @@
+/home/wizel/rust_projects/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb: src/main.rs
+
+/home/wizel/rust_projects/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/wizel/rust_projects/hello_cargo/target/debug/hello_cargo: /home/wizel/rust_projects/hello_cargo/src/main.rs
