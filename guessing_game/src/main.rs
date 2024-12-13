e4ccd97250b7f5774e416b6538d1370148e46974
wizel10 <wizel10@gmail.com>
2024-12-13 13:17:15 +0100

    2024-12-13 13:17:18

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,8 +4,10 @@ fn main() {
     // ask the number
     println!("Guess the number");
     println!("Pls input your best guess:");
+
     let mut guess = String::new();
 
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
     io::stdin()
         .read_line(&mut guess)
         .expect( "Failed to read line");
